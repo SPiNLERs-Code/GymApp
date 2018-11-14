fbccdff70be392405f1e8d1b001ca3b4b7699d06
Bryan Albrecht <bryanalbrecht@Tijanas-MacBook-Pro.local>
2018-11-14 14:46:55 +0100

    Initial commit

---
+++ GymApp/ViewControllers/EnduranceViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EnduranceViewController.swift
+//  GymApp
+//
+//  Created by Bryan Albrecht on 14.11.18.
+//  Copyright © 2018 Bryan Albrecht. All rights reserved.
+//
+
+import UIKit
+
+class EnduranceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ GymApp/ViewControllers/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  GymApp
+//
+//  Created by Bryan Albrecht on 14.11.18.
+//  Copyright © 2018 Bryan Albrecht. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
