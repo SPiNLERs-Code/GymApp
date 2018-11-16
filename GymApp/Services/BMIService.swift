6abd2a451d1400e6665f765c9c571c88f305daf3
Bryan Albrecht <bryanalbrecht@Tijanas-MacBook-Pro.local>
2018-11-16 11:50:03 +0100

    Added BMI Service

---
+++ GymApp/Services/BMIService.swift
@@ -0,0 +1,15 @@
+//
+//  BMIService.swift
+//  GymApp
+//
+//  Created by Bryan Albrecht on 16.11.18.
+//  Copyright © 2018 Bryan Albrecht. All rights reserved.
+//
+
+struct BMIService{
+    
+    func calculateBMI(height: Int, weight: Int) -> Double{
+        
+        return 0.0
+    }
+}
