71387799619d1973aaabfe40483b7f7096ed73fb
Bryan Albrecht <bryanalbrecht@Tijanas-MacBook-Pro.local>
2018-11-20 15:29:00 +0100

    Added camera access

---
+++ GymApp/Services/CameraService.swift
@@ -0,0 +1,14 @@
+//
+//  CameraService.swift
+//  GymApp
+//
+//  Created by Bryan Albrecht on 20.11.18.
+//  Copyright © 2018 Bryan Albrecht. All rights reserved.
+//
+
+import UIKit
+
+class CameraService: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+
+}
