f67768c241828a5df1bf01426e9307e93d951e39
Bryan Albrecht <bryanalbrecht@Tijanas-MacBook-Pro.local>
2018-11-21 11:38:56 +0100

    Added Location

--- GymApp/ViewControllers/EnduranceViewController.swift
+++ GymApp/ViewControllers/EnduranceViewController.swift
@@ -7,12 +7,108 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class EnduranceViewController: UIViewController {
+class EnduranceViewController: UIViewController, CLLocationManagerDelegate{
 
+    @IBOutlet weak var coordinatesLabel: UILabel!
+
+    let locationManager = CLLocationManager()
+    var locationStatus = CLAuthorizationStatus.notDetermined
+    
+    
+    @IBAction func changeLocationSetting(_ sender: Any) {
+        
+        let mySwitch = sender as! UISwitch
+        
+        if mySwitch.isOn{
+            startReceivingLocationChanges()
+        }else{
+            stopReceivingLocationChanges()
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateLocationAccess()
+        if locationStatus == .authorizedAlways || locationStatus == .authorizedWhenInUse{
+            setupLocationManager()
+            startReceivingLocationChanges()
+        }
+        
+    }
+    
+    func setupLocationManager(){
+        let authorizationStatus = CLLocationManager.authorizationStatus()
+        if authorizationStatus != .authorizedWhenInUse && authorizationStatus != .authorizedAlways {
+            // User has not authorized access to location information.
+            return
+        }
+        // Do not start services that aren't available.
+        if !CLLocationManager.locationServicesEnabled() {
+            // Location services is not available.
+            return
+        }
+        // Configure and start the service.
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.distanceFilter = 100.0  // In meters.
+        locationManager.delegate = self
+    }
+    
+    func updateLocationAccess(){
+        locationManager.delegate = self
+        
+        switch CLLocationManager.authorizationStatus() {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .restricted, .denied:
+            locationStatus = .denied
+            break
+            
+        case .authorizedWhenInUse:
+            locationStatus = .authorizedWhenInUse
+            break
+            
+        case .authorizedAlways:
+            locationStatus = .authorizedAlways
+            break
+        }
+    }
+    
+    func startReceivingLocationChanges() {
+        locationManager.startUpdatingLocation()
+    }
+    
+    func stopReceivingLocationChanges() {
+        
+        locationManager.stopUpdatingLocation()
+        resetCooridatesLabel()
+    }
+    
+    func resetCooridatesLabel(){
+        upDateCooridnatesLabel(latitude: "XXX", longitude: "XXX")
+    }
+    
+    func locationManager(_ manager: CLLocationManager,  didUpdateLocations locations: [CLLocation]) {
+        let lastLocation = locations.last!
+        upDateCooridnatesLabel(latitude: String(lastLocation.coordinate.latitude), longitude: String(lastLocation.coordinate.longitude))
+    }
+    
+    func upDateCooridnatesLabel(latitude: String, longitude: String){
+        coordinatesLabel.text = "\(latitude) / \(longitude)"
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        if let error = error as? CLError, error.code == .denied {
+            // Location updates are not authorized.
+            stopReceivingLocationChanges()
+            return
+        }
+        // Notify the user of any errors.
     }
 
 
--- GymApp/ViewControllers/StrenghtViewController.swift
+++ GymApp/ViewControllers/StrenghtViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class StrenghtViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    private let imageService = ImageService()
+    
     @IBOutlet weak var imageView: UIImageView!
     
     
@@ -25,13 +28,16 @@ class StrenghtViewController: UIViewController, UIImagePickerControllerDelegate,
         
         let actionSheet = UIAlertController(title: "PhotoSource", message: "Choose a source", preferredStyle: .actionSheet)
         
-        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler:  {(action:UIAlertAction) in
-            
-            if UIImagePickerController.isSourceTypeAvailable(.camera) {
-                imagePickerController.sourceType = .camera
-                self.present(imagePickerController, animated: true,completion: nil)
-            }
-        }))
+        if AVCaptureDevice.authorizationStatus(for: .video) !=  .denied {
+            actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler:  {(action:UIAlertAction) in
+                
+                if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                    imagePickerController.sourceType = .camera
+                    self.present(imagePickerController, animated: true,completion: nil)
+                }
+            }))
+        }
+        
         
         actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler:  {(action:UIAlertAction) in
             imagePickerController.sourceType = .photoLibrary
@@ -46,14 +52,34 @@ class StrenghtViewController: UIViewController, UIImagePickerControllerDelegate,
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
-        
-        imageView.image = image
+        let shrinkedImage = imageService.convertTosquare(image: image)
+        imageView.image = shrinkedImage
         picker.dismiss(animated: true, completion: nil)
         
         }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        let imageView = sender.view as! UIImageView
+        let newImageView = UIImageView(image: imageView.image)
+        newImageView.frame = UIScreen.main.bounds
+        newImageView.backgroundColor = .black
+        newImageView.contentMode = .scaleAspectFit
+        newImageView.isUserInteractionEnabled = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissFullscreenImage))
+        newImageView.addGestureRecognizer(tap)
+        self.view.addSubview(newImageView)
+        self.navigationController?.isNavigationBarHidden = true
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    @objc func dismissFullscreenImage(_ sender: UITapGestureRecognizer) {
+        self.navigationController?.isNavigationBarHidden = false
+        self.tabBarController?.tabBar.isHidden = false
+        sender.view?.removeFromSuperview()
     }
 
 }
